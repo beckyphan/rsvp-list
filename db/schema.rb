735699af23062e4704c51b87192680f763b59cc4
Becky Phan <beckyphan@live.com>
2021-02-14 12:57:23 -0800

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_202800) do
+ActiveRecord::Schema.define(version: 2021_02_14_205640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_202800) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "party_id", null: false
+    t.integer "plusones"
     t.index ["party_id"], name: "index_guests_on_party_id"
   end
 
