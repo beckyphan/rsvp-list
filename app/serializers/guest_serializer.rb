be933da225c8c00096bd33fa7fa13e65851498b2
Becky Phan <beckyphan@live.com>
2021-02-15 13:42:46 -0800

    added updated_at to guest serializer

--- app/serializers/guest_serializer.rb
+++ app/serializers/guest_serializer.rb
@@ -1,4 +1,4 @@
 class GuestSerializer
   include JSONAPI::Serializer
-  attributes :fname, :lname, :attending, :shuttle, :hotel, :notes, :plusones, :party_id
+  attributes :fname, :lname, :attending, :shuttle, :hotel, :notes, :plusones, :party_id, :updated_at
 end
